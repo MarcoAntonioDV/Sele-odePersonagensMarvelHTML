0a027cfe2cd93e17188cf53256c55e9ab596eefd
Marco Antonio <marcoantonio.a.davanzo2001@hotmail.com>
2024-10-26 23:14:49 -0300

    início de js

---
+++ src/Js/index.js
@@ -0,0 +1,17 @@
+/*
+    OBJETIVO 1: Quando passar o mouse em cima do personagem temos que: 
+    -colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
+
+    -retirar a classe selecionado do personagem anterior que está selecionado
+
+    OBJETIVO 2: Quando passar o mouse em cima do personagem temos que:
+    -trocar a imagem grande do jogador 1 pelo personagem que o mouse estará em cima
+
+    -trocar o nome do personagem grande do jogador 1 pelo outro que o mouse estará em cima
+*/
+
+// OBJETIVO 1: Quando passar o mouse em cima do personagem temos que: 
+//     -colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animação nele
+
+//     -retirar a classe selecionado do personagem anterior que está selecionado
+
